a647a066cddb4714e1a6267a8bc51407b8be25a5
Henrique Tosta <glhu@gft.com>
2019-09-10 17:20:53 -0300

    add saveTodo

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,10 @@ admin.initializeApp({
   databaseURL: "https://api-with-firebase.firebaseio.com"
 });
 
+// configura db
 const db = admin.firestore();
 
-// EXAMPLE
-const todo = {
-  title: 'Javascript é massa!',
-  description: 'Estudar Javascript',
-  color: 'Brown',
-  status: 'DOING',
-}
-
+// busca todos
 exports.getTodos = functions.https.onRequest(async (request, response) => {
   response.set('Access-Control-Allow-Origin', '*');
 
@@ -27,6 +21,23 @@ exports.getTodos = functions.https.onRequest(async (request, response) => {
     response.status(200).json({
       todos: snapshot.docs.map((doc) => doc.data())
     });
+
+  } catch(e) {
+    response.status(500).json({ error: e });
+  }
+});
+
+// salva todo
+exports.saveTodo = functions.https.onRequest(async (request, response) => {
+  if (request.method !== 'POST') {
+    response.status(403);
+  }
+
+  response.set('Access-Control-Allow-Origin', '*');
+
+  try {
+    const docRef = await db.collection('todos').add(request.body);
+    response.status(200).json({ id: docRef.id });
 
   } catch(e) {
     response.status(500).json({ error: e });
